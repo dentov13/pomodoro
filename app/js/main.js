113b2d1bcf32ea2a35ccf93b1fc940cb21c31944
dentov13 <tovstokhatko.denis@gmail.com>
2016-12-12 22:13:32 +0200

    Start to push JS logic

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,32 @@
-"use strict";
+var activeTime, pomodoro
+var breakTimeSec = 300 // 300 sec = 5 min
+var workTimeSec = 1500 // 1500 sec = 25 min
 
-var name = "Den";
+function renderTime (time) {
+	var minutes = Math.floor(time / 60)
+	var seconds = time % 60
 
-console.log(name);
+	if (seconds.toString().length === 1) seconds = '0' + seconds
+	time = minutes + ":" + seconds
+	window.time.innerHTML = time
+	document.title = time + ' - Pomodoro'
+}
+
+function toggleStatusIcon (pause) {
+	if (pause) window.statusIcon.innerHTML = '❙❙'
+	else window.statusIcon.innerHTML = '▶'
+}
+
+function working () {
+	return window.tomato.innerHTML === 'Work'
+}
+
+function toggleType (type) {
+	if (working()) {
+		activeTime = breakTimeSec
+		window.tomato.innerHTML = 'Break'
+	} else {
+		activeTime = workTimeSec
+		window.tomato.innerHTML = 'Work'
+	}
+}
