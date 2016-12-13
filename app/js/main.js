c011b550a41800ad5a27ae3c8d8fbf9bb40a543a
dentov13 <tovstokhatko.denis@gmail.com>
2016-12-13 02:03:41 +0200

    add scripts

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,120 @@
-var activeTime, pomodoro
-var breakTimeSec = 300 // 300 sec = 5 min
-var workTimeSec = 1500 // 1500 sec = 25 min
+'use strict';
 
-function renderTime (time) {
-	var minutes = Math.floor(time / 60)
-	var seconds = time % 60
+var activeTime, pomodoro;
+var breakTimeSec = 300; // 300 sec = 5 min
+var workTimeSec = 1500; // 1500 sec = 25 min
+var beepAudio = new Audio("http://www.soundjay.com/button/sounds/beep-08b.mp3")
 
-	if (seconds.toString().length === 1) seconds = '0' + seconds
-	time = minutes + ":" + seconds
-	window.time.innerHTML = time
-	document.title = time + ' - Pomodoro'
+function beep() {
+	beepAudio.play()
 }
 
-function toggleStatusIcon (pause) {
-	if (pause) window.statusIcon.innerHTML = '❙❙'
-	else window.statusIcon.innerHTML = '▶'
+function renderTime(time) {
+	var minutes = Math.floor(time / 60);
+	var seconds = time % 60;
+
+	if (seconds.toString().length === 1) seconds = '0' + seconds;
+	time = minutes + ":" + seconds;
+	window.time.innerHTML = time;
+	document.title = time + ' - Pomodoro';
 }
 
-function working () {
-	return window.tomato.innerHTML === 'Work'
+function toggleStatusIcon(pause) {
+	if (pause) window.statusIcon.innerHTML = '❙❙';else window.statusIcon.innerHTML = '▶';
 }
 
-function toggleType (type) {
+function working() {
+	return window.tomato.innerHTML === 'Work';
+}
+
+function toggleType(type) {
 	if (working()) {
-		activeTime = breakTimeSec
-		window.tomato.innerHTML = 'Break'
+		activeTime = breakTimeSec;
+		window.tomato.innerHTML = 'Break';
 	} else {
-		activeTime = workTimeSec
-		window.tomato.innerHTML = 'Work'
+		activeTime = workTimeSec;
+		window.tomato.innerHTML = 'Work';
 	}
 }
+
+function clearPomodoro() {
+	window.clearInterval(pomodoro);
+	pomodoro = null;
+}
+
+function toggleActive() {
+	toggleStatusIcon(!pomodoro)
+
+	if (!pomodoro) {
+		pomodoro = window.setInterval(() => {
+			activeTime--
+
+			if (activeTime <= 3) beep()
+			if (activeTime < 0) toggleType()
+
+			renderTime(activeTime)
+		}, 1000)
+	} else {
+		clearPomodoro()
+	}
+}
+
+function resetTimer() {
+	activeTime = workTimeSec
+
+	if (pomodoro) clearPomodoro()
+
+	if (!working()) toggleType()
+	toggleStatusIcon(false)
+	renderTime(activeTime)
+}
+
+function timeIncDec(click) {
+	if (pomodoro) return
+
+	var id = click.target.innerHTML
+	var x = click.target.className
+
+	if (id === '+') {
+		window[x + 'Sec'] += 60
+	} else if (id === '-' && window[x + 'Sec'] > 60) {
+		window[x + 'Sec'] -= 60
+	} else {
+		return
+	}
+
+	window[x].innerHTML = Math.floor(window[x + 'Sec'] / 60)
+
+	activeTime = workTimeSec
+	renderTime(activeTime)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	resetTimer()
+	renderTime(activeTime)
+	window.timer.addEventListener('click', toggleActive)
+	window.reset.addEventListener('click', resetTimer)
+	window.skip.addEventListener('click', () => {
+		toggleType()
+		renderTime(activeTime)
+	})
+
+	Array.from(document.getElementsByClassName('timeControl'))
+	     .forEach((x) => x.addEventListener('click', timeIncDec))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp'),
     babel = require("gulp-babel"),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync'),
-    del = require('del'),
     autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
@@ -28,25 +27,10 @@ gulp.task("scripts", function () {
     .pipe(gulp.dest("app/js"));
 });
 
-gulp.task('clean', function () {
-	return del.sync('dist');
-});
-
 gulp.task('watch', ['browser-sync', 'sass', 'scripts'], function() {
   gulp.watch(['app/sass/**/*.sass'], ['sass']);
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
-});
-
-gulp.task('build', ['clean', 'sass', 'scripts'], function () {
-  var buildCss = gulp.src('app/css/main.css')
-  	.pipe(gulp.dest('dist/css'));
-
-  var buildJs = gulp.src('app/js/**/*')
-  	.pipe(gulp.dest('dist/js'));
-
-  var buildHtml = gulp.src('app/*.html')
-  	.pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['watch']);
